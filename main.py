15d010efb3dcc4f8dfce0a778b7379a9087fc5b0
unknown <abhayshukla973@gmail.com>
2026-04-04 12:17:15 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.DataScience_Project import logger
 from src.DataScience_Project.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.DataScience_Project.pipeline.data_validation import DataValidationTrainingPipeline
+
 
 STAGE_NAME="Data Ingestion Stage"
-
-
 try:
         logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<') 
         obj= DataIngestionTrainingPipeline()
@@ -12,3 +12,13 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e  
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
