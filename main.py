2ac0fddaf5c1d76869a4c8d95eb3621423b69333
unknown <abhayshukla973@gmail.com>
2026-04-06 11:46:10 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from src.DataScience_Project.pipeline.model_evaluation import ModelEvaluationTrainingPipeline
 from src.DataScience_Project import logger
 from src.DataScience_Project.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from src.DataScience_Project.pipeline.data_validation import DataValidationTrainingPipeline
 from src.DataScience_Project.pipeline.data_transfromation import DataTransformationTrainingPipeline
+from src.DataScience_Project.pipeline.model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion Stage"
@@ -30,6 +32,26 @@ try:
         data_transformation = DataTransformationTrainingPipeline()
         data_transformation.initiate_data_transformation()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")    
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME="Model Trainer Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_transformation = ModelTrainerTrainingPipeline()
+        data_transformation.initiate_model_training()   
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")    
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
--- src/DataScience_Project/config/configuration.py
+++ src/DataScience_Project/config/configuration.py
@@ -1,6 +1,6 @@
 from src.DataScience_Project.constants import *
 from src.DataScience_Project.utils.common import read_yaml, create_directories
-from src.DataScience_Project.entity.config_entity import (DataIngestionConfig, DataTransformationConfig,DataValidationConfig)
+from src.DataScience_Project.entity.config_entity import (DataIngestionConfig, DataTransformationConfig,DataValidationConfig,ModelTrainerConfig,ModelEvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -54,3 +54,42 @@ class ConfigurationManager:
         )
         
         return data_transformation_config
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
+        )
+
+        return model_trainer_config
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config=self.config.model_evaluation
+        params=self.params.ElasticNet
+        schema=self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config=ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/ashukla966/Datascience_Project.mlflow"
+
+
+        )
+        return model_evaluation_config    
